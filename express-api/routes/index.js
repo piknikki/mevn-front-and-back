3c9a7bb1888e5213f29d24e835c0a0aaa3419648
Nikki Petersen <piknik@me.com>
2022-06-10 05:36:02 -0600

    Update route endpoints

--- express-api/routes/index.js
+++ express-api/routes/index.js
@@ -4,7 +4,36 @@ const cors = require('cors')
 
 /* GET home page. */
 router.get('/', cors(), function (req, res) {
-  res.json({ message: "it's alive!! 👾👾👾👾👾👾" })
+  res.json({ message: "it's alive (things)!! 👾👾👾👾👾👾" })
+})
+
+router.get('/users', cors(), function (req, res) {
+  res.json({
+    code: 200,
+    message: "user success!! 🥓 🍄 😱",
+    user: {
+      "id": "42",
+      "firstName": "Tricia",
+      "lastName": "McMillan",
+      "email": "Trillian@HG2G.com"
+    }
+  })
 })
 
 module.exports = router
+
+//const users = [
+//   {
+//     "id": "42",
+//     "firstName": "Tricia",
+//     "lastName": "McMillan",
+//     "email": "Trillian@HG2G.com"
+//   },
+//   {
+//     "id": "42-42",
+//     "firstName": "Leia",
+//     "lastName": "Organa",
+//     "email": "MyOnlyHope@rebels.com"
+//   }
+// ]
+
